295807da9fe5ed8f67469f9a651422490aa48b6c
devadharshini12 <101504240+devadharshini12@users.noreply.github.com>
2023-01-11 22:30:06 +0530

    Add files via upload

---
+++ # Longest non in sub array.py
@@ -0,0 +1,16 @@
+# Longest non in sub array
+def printMax(arr, n, k): 
+    max = 0
+    
+    for i in range(n - k + 1): 
+        max = arr[i] 
+        for j in range(1, k): 
+            if arr[i+j] > max: 
+                max = arr[i + j] 
+        print(str(max) + " ", end = "") 
+  
+
+    arr = [10,5,2,7,8,7] 
+    n = len(arr) 
+    k = 3
+    printMax(arr, n, k)
